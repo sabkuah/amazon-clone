9b3715671d22827fc4c33944133938116a66ca91
Sabrina Kuah <sabrina@Sabrinas-MacBook-Pro.local>
2021-01-01 21:00:15 -0800

    Firebase config added

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBn_LCrggpnh_SBryxdunn1s2ojCB9YnzI",
+  authDomain: "sabkuah-961f5.firebaseapp.com",
+  projectId: "sabkuah-961f5",
+  storageBucket: "sabkuah-961f5.appspot.com",
+  messagingSenderId: "1026399094433",
+  appId: "1:1026399094433:web:61860aad333c5957b6d2e0",
+};
