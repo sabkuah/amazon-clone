5e0e97eeaa16e0608e81eea5b7b119fc53dca206
Sabrina Kuah <sabrina@Sabrinas-MacBook-Pro.local>
2021-01-04 23:45:41 -0800

    cloud backend deployed

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/sabkuah-961f5/us-central1/api", //The API (cloud function) URL
+  baseUrl: "https://us-central1-sabkuah-961f5.cloudfunctions.net/api",
+  //baseURL: "http://localhost:5001/sabkuah-961f5/us-central1/api", //The API (cloud function) URL
 });
 
 export default instance;
